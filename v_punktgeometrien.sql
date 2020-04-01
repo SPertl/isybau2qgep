e01a50b3527bff50370def5c4dc1e3f6e9db04b9
SPertl <38438454+SPertl@users.noreply.github.com>
2020-04-01 15:19:14 +0200

    nicht benötigt

--- v_punktgeometrien.sql
+++
@@ -1,8 +0,0 @@
---Knoten: Bildung der Punktgeometrien, Achtung: nicht deckungsgleich mit Abwassertechnischen Anlagen, mehrer Punkte pro Anlage (SMP, DMP, etc.) möglich!
-CREATE OR REPLACE VIEW isy_in.v_punktgeometrien AS 
- SELECT kp.ogc_fid, kp.ogr_pkid, kp.parent_ogr_pkid,
-    kp.rechtswert, kp.hochwert, kp.punkthoehe,
-    kp.punktattributabwasser,
-    kp.lagegenauigkeitsstufe, kp.hoehengenauigkeitsstufe,
-    ST_SetSRID(ST_MakePoint(kp.rechtswert, kp.hochwert), 4647)::geometry(Point,4647) AS p_geom_2d
-   FROM isy_in.identi_datenk_stammd_abwassanlage_geomet_geomet_knoten_punkt kp;
