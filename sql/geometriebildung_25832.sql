978402f5fd412b366902f8157a13684bc0e128ca
SPertl <38438454+SPertl@users.noreply.github.com>
2020-04-16 09:47:59 +0200

    Update geometriebildung_25832.sql

--- sql/geometriebildung_25832.sql
+++ sql/geometriebildung_25832.sql
@@ -28,16 +28,17 @@ CREATE OR REPLACE VIEW qgisybau.v_liniengeometrien AS
    FROM isybau.identi_datenk_stammd_abwassanlage_geomet_geomet_kanten_kante kk
 -- sowie der Polylinien aus mehreren Abschnitten
 UNION ALL
- SELECT ( SELECT p_1.parent_ogr_pkid
-           FROM isybau.ident_daten_stammd_abwassanlage_geomet_geomet_polygo_polygon p_1
-          WHERE pk.parent_ogr_pkid::text = p_1.ogr_pkid::text) AS parent_ogr_pkid,
-    st_setsrid(st_makeline(st_makeline(st_makepoint(pk.start_rechtswert, pk.start_hochwert), 
-                                       st_makepoint(pk.ende_rechtswert, pk.ende_hochwert))), 25832)::geometry(LineString,25832) AS l_geom_2d
-   FROM isybau.ident_daten_stamm_abwasanlag_geome_geome_polygo_polygo_kante pk,
-    isybau.ident_daten_stammd_abwassanlage_geomet_geomet_polygo_polygon p
-  WHERE pk.parent_ogr_pkid::text = p.ogr_pkid::text 
-    AND p.polygonart = 3 -- V105: 3=Polylinie eines Objektes (offen)
-  GROUP BY pk.parent_ogr_pkid;
+SELECT 
+	p.parent_ogr_pkid,
+	ST_UNION(st_setsrid(st_makeline(st_makepoint(pk.start_rechtswert, pk.start_hochwert), 
+									st_makepoint(pk.ende_rechtswert, pk.ende_hochwert)), 25832)::geometry(LineString,25832)) AS l_geom_2d
+FROM
+	isybau.ident_daten_stamm_abwasanlag_geome_geome_polygo_polygo_kante pk
+LEFT JOIN
+	isybau.ident_daten_stammd_abwassanlage_geomet_geomet_polygo_polygon p
+	ON pk.parent_ogr_pkid::text = p.ogr_pkid::text
+GROUP BY p.parent_ogr_pkid ORDER BY p.parent_ogr_pkid;
+
 
 -- KNOTEN: Bildung von Flächengeometrien
 CREATE OR REPLACE VIEW qgisybau.v_flaechengeometrien AS 
